8e758450242fb3cd64a3642ece342bdcee1a8adc
Kqan1 <kaaneskimarmara@gmail.com>
2024-02-20 12:04:31 +0300

    tutustur

---
+++ content.js
@@ -0,0 +1,16 @@
+// Tarayıcı sayfa yüklendiğinde tetiklenecek olan fonksiyon
+function onPageLoad() {
+    const willDelete = document.querySelectorAll('p, span, a, pre, h1, h2, h3, h4, h5, h6');
+    console.log(willDelete);
+
+    willDelete.forEach(function(element) {
+
+        
+        const random = Math.random() * 100;
+
+        element.textContent = random > 10 ? "oşşhhh" : "hurrr"
+        console.log(element.textContent);
+    });
+};
+
+window.onload = onPageLoad;
