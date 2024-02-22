6af786ccfcf9cc01cd8b7276ff91c28f63369878
Kqan1 <kaaneskimarmara@gmail.com>
2024-02-22 14:33:45 +0300

    hurr

--- content.js
+++ content.js
@@ -1,16 +1,26 @@
-// Tarayıcı sayfa yüklendiğinde tetiklenecek olan fonksiyon
+const Texts = document.querySelectorAll('p, span, a, pre, li, ol, h1, h2, h3, h4, h5, h6');
+const img = document.querySelectorAll("img");
+const div = document.querySelectorAll("div");
+const words = ["hurr", "oşh", "tutuş", "garra", "ırrşş", "gırrr", "mutuş", "tutuşullah", "şutum", "qral"];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
 function onPageLoad() {
-    const willDelete = document.querySelectorAll('p, span, a, pre, li, ol, h1, h2, h3, h4, h5, h6');
-    console.log(willDelete);
-
-    willDelete.forEach(function(element) {
-
-        
-        const random = Math.random() * 100;
-
-        element.textContent = random > 10 ? "oşşhhh" : "hurrr"
-        console.log(element.textContent);
-    });
+    setInterval(()=>{
+        div.forEach(div => div.style.backgroundImage = "url('https://i.ytimg.com/vi/dun23sVIIt8/sddefault.jpg')")
+        img.forEach(img => img.src = "https://i.ytimg.com/vi/dun23sVIIt8/sddefault.jpg");
+        Texts.forEach(function(element) {
+            const textLength = element.textContent.split(" ").length;
+            const newText = [];
+            for (let i = 0; i < textLength; i++) {
+                newText.push(words[getRandomInt(words.length)]);
+            }
+            element.textContent = newText.join(" ");
+            element.style.color = "white";
+        });
+    }, 100);
 };
 
 window.onload = onPageLoad;
